4355071ed2e3769bfb43e03eda78a57b5ee37d2f
AkinaLTS <108107785+AkinaLTS@users.noreply.github.com>
2025-05-23 00:24:10 +0800

    add binutils-aarch64-linux-gnu aria2

--- installDeps.sh
+++ installDeps.sh
@@ -2,4 +2,4 @@
 
 sudo apt update -y
 sudo apt upgrade -y
-sudo apt install bc bison build-essential ccache curl flex g++-multilib gcc-multilib git git-lfs gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev libelf-dev liblz4-tool libncurses5 libncurses5-dev libsdl1.2-dev libssl-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev adb fastboot binutils-arm-linux-gnueabi -y
+sudo apt install bc bison build-essential ccache curl flex g++-multilib gcc-multilib git git-lfs gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev libelf-dev liblz4-tool libncurses5 libncurses5-dev libsdl1.2-dev libssl-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev adb fastboot binutils-arm-linux-gnueabi binutils-aarch64-linux-gnu aria2 -y
