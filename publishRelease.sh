fd32263584e50100da7ab0790f5841483511b62a
AkinaLTS <108107785+AkinaLTS@users.noreply.github.com>
2025-05-23 00:56:35 +0800

    add branch

--- publishRelease.sh
+++ publishRelease.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/bash
 
 TIME="$(date +"%Y-%m-%d_%H:%M:%S")"
-gh release create ${TIME} --notes "Build time: ${TIME}" ./out/arch/arm64/boot/*Image*
+gh release create ${TIME} --notes "Build time: ${TIME}
+Branch: $(git branch)" ./out/arch/arm64/boot/*Image*
