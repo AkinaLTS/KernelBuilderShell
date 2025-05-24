c78167b994037b7883037ae66869d6bbcda40d1e
AkinaLTS <108107785+AkinaLTS@users.noreply.github.com>
2025-05-24 17:34:21 +0800

    fix

--- publishRelease.sh
+++ publishRelease.sh
@@ -4,7 +4,7 @@
 TIME="$(date +"%Y-%m-%d_%H:%M:%S")"
 TIME_ID="$(date +"%Y-%m-%d-%H-%M-%S")"
 gh release create ${TIME_ID} --title "${TIME}" --notes "Build time: ${TIME} \ 
-    Branch: \
-    $(git branch --show-current) \
-    Commit Message: \
+    Branch: \ 
+    $(git branch --show-current) \ 
+    Commit Message: \ 
     $(git show --no-patch)" ./out/arch/arm64/boot/*Image*
