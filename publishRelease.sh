a1d9e37e03c133a335d4c7f533956855539150f0
AkinaLTS <108107785+AkinaLTS@users.noreply.github.com>
2025-05-24 17:07:05 +0800

    more information about release

--- publishRelease.sh
+++ publishRelease.sh
@@ -3,6 +3,8 @@
 
 TIME="$(date +"%Y-%m-%d_%H:%M:%S")"
 TIME_ID="$(date +"%Y-%m-%d-%H-%M-%S")"
-gh release create ${TIME_ID} --title "${TIME}" --notes "Build time: ${TIME}
-Branch: 
-$(git branch)" ./out/arch/arm64/boot/*Image*
+gh release create ${TIME_ID} --title "${TIME}" --notes "Build time: ${TIME} \ 
+    Branch: \
+    $(git branch --show-current) \
+    Commit Message: \
+    $(git show --no-patch)" ./out/arch/arm64/boot/*Image*
