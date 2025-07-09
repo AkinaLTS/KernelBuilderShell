ce88a0c3d92d726116c9452fe6561a2136f8385d
AkinaLTS <108107785+AkinaLTS@users.noreply.github.com>
2025-07-09 14:37:37 +0800

    fix

--- installDeps.sh
+++ installDeps.sh
@@ -3,4 +3,4 @@
 
 sudo apt update -y
 sudo apt upgrade -y
-sudo apt install bc bison build-essential ccache curl flex g++-multilib gcc-multilib git git-lfs gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev libelf-dev liblz4-tool libncurses libncurses-dev libsdl1.2-dev libssl-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev adb fastboot binutils-arm-linux-gnueabi binutils-aarch64-linux-gnu aria2 -y
+sudo apt install bc bison build-essential ccache curl flex g++-multilib gcc-multilib git git-lfs gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev libelf-dev liblz4-tool libncurses libncurses-dev libsdl1.2-dev libssl-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev adb fastboot binutils-arm-linux-gnueabi binutils-aarch64-linux-gnu wget cpio -y
