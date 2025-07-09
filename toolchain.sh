894a14e0dac3c79873bf8e197090ac1e10fa0a9e
AkinaLTS <108107785+AkinaLTS@users.noreply.github.com>
2025-07-09 14:05:30 +0800

    fix bugs
    tmp
    
    tmp

--- toolchain.sh
+++ toolchain.sh
@@ -4,4 +4,7 @@
 # clang main-kernel clang-stable
 TCURL="https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/heads/main-kernel/clang-r547379.tar.gz"
 
-aria2c "${TCURL}"
+wget "${TCURL}" -O ~/tc.tgz
+
+mkdir -p ~/tc
+tar xvf ~/tc.tgz -C ~/tc
