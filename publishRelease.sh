e2330f2a75964022b7a028e3f89ff983f5a17d09
AkinaLTS <108107785+AkinaLTS@users.noreply.github.com>
2025-07-16 00:43:23 +0800

    dev

--- publishRelease.sh
+++ publishRelease.sh
@@ -3,8 +3,28 @@
 
 TIME="$(date +"%Y-%m-%d_%H:%M:%S")"
 TIME_ID="$(date +"%Y-%m-%d-%H-%M-%S")"
+FILES=""
+
+if [[ -f "./out/arch/arm64/boot/*Image*" ]]; then
+    echo "Find Image."
+    FILES+="./out/arch/arm64/boot/*Image*"
+else
+    echo "No Image file."
+fi
+if [[ -f "out/arch/arm64/boot/*dtb*" ]]; then
+    echo "Find dtb dtbo."
+    FILES+="./out/arch/arm64/boot/*dtb*"
+else
+    echo "No dtb dtbo files."
+fi
+
+if [[ -n "${FILES}" ]]; then
+    echo "Nothing can be uploaded. Exited."
+    exit 1
+fi
+
 gh release create ${TIME_ID} --title "${TIME}" --notes "Build time: ${TIME} \ 
     Branch: \ 
     $(git branch --show-current) \ 
     Commit Message: \ 
-    $(git show --no-patch)" ./out/arch/arm64/boot/*Image*
+    $(git show --no-patch)" ${FILES}
