5b433a95513fc8471ea23f86fc7b61809ff836b2
AkinaLTS <108107785+AkinaLTS@users.noreply.github.com>
2025-07-15 23:15:28 +0800

    add tmux as dep

--- installDeps.sh
+++ installDeps.sh
@@ -3,4 +3,4 @@
 
 sudo apt update -y
 sudo apt upgrade -y
-sudo apt install bc bison build-essential ccache curl flex g++-multilib gcc-multilib git git-lfs gnupg gperf imagemagick lib32readline-dev lib32z1-dev libelf-dev liblz4-tool libncurses-dev libsdl1.2-dev libssl-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev adb fastboot binutils-arm-linux-gnueabi binutils-aarch64-linux-gnu wget cpio -y
+sudo apt install bc bison build-essential ccache curl flex g++-multilib gcc-multilib git git-lfs gnupg gperf imagemagick lib32readline-dev lib32z1-dev libelf-dev liblz4-tool libncurses-dev libsdl1.2-dev libssl-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev adb fastboot binutils-arm-linux-gnueabi binutils-aarch64-linux-gnu wget cpio tmux -y
