634d6298575a173ebe5fc7e8aef467484e1614e3
AkinaLTS <108107785+AkinaLTS@users.noreply.github.com>
2025-07-15 21:33:53 +0800

    new tool

---
+++ publishErrorLog.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+# Copyright (c) 2025 Arkria. All Rights Reserved.
+
+TIME="$(date +"%Y-%m-%d_%H:%M:%S")"
+TIME_ID="$(date +"%Y-%m-%d-%H-%M-%S")"
+gh release create ${TIME_ID} --title "Error: ${TIME}" --notes "Build time: ${TIME} \ 
+    Branch: \ 
+    $(git branch --show-current) \ 
+    Commit Message: \ 
+    $(git show --no-patch)" ./error.log
